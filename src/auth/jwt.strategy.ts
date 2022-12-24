1ba67e2dffe5202d88b5d84e6e233b4a25e78f52
이종은 <bellsilver7@gmail.com>
2022-12-24 14:37:02 +0900

    guard 테스트

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
+    super({
+      secretOrKey: 'Secret1234',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload) {
+    const { username } = payload;
+    const user = this.userRepository.findOneBy({ username });
+
+    if (!user) throw new UnauthorizedException();
+
+    return user;
+  }
+}
